01f1aeb6dd77d519d4ca27cec86c608599821447
lazaina14472 <lazainaandrianasolonirina14472@gmail.com>
2026-09-05 17:37:05 +0200

    Format API endpoints for better readability

--- API/API.js
+++ API/API.js
@@ -1,23 +1,35 @@
-export const API_login = 'https://e-commerce-simple-backend.onrender.com/login';
+export const API_login =
+    'https://e-commerce-simple-backend.onrender.com/login';
 
-export const API_ajouterProduits = 'https://e-commerce-simple-backend.onrender.com/ajouterProduits';
+export const API_ajouterProduits =
+    'https://e-commerce-simple-backend.onrender.com/ajouterProduits';
 
-export const API_displayProduits = 'https://e-commerce-simple-backend.onrender.com/displayProduits';
+export const API_displayProduits =
+    'https://e-commerce-simple-backend.onrender.com/displayProduits';
 
-export const API_deleteProduits = 'https://e-commerce-simple-backend.onrender.com/deleteProduits';
+export const API_deleteProduits =
+    'https://e-commerce-simple-backend.onrender.com/deleteProduits';
 
-export const API_modifierProduits = 'https://e-commerce-simple-backend.onrender.com/modifierProduits';
+export const API_modifierProduits =
+    'https://e-commerce-simple-backend.onrender.com/modifierProduits';
 
-export const API_modifierImage = 'https://e-commerce-simple-backend.onrender.com/modifierImage';
+export const API_modifierImage =
+    'https://e-commerce-simple-backend.onrender.com/modifierImage';
 
-export const API_commenderProduits = 'https://e-commerce-simple-backend.onrender.com/commenderProduits';
+export const API_commenderProduits =
+    'https://e-commerce-simple-backend.onrender.com/commenderProduits';
 
-export const API_displayCommandeClients = 'https://e-commerce-simple-backend.onrender.com/displayCommandeClients';
+export const API_displayCommandeClients =
+    'https://e-commerce-simple-backend.onrender.com/displayCommandeClients';
 
-export const API_deleteCommandeClients = 'https://e-commerce-simple-backend.onrender.com/deleteCommandeClients';
+export const API_deleteCommandeClients =
+    'https://e-commerce-simple-backend.onrender.com/deleteCommandeClients';
 
-export const API_validerCommandeClients = 'https://e-commerce-simple-backend.onrender.com/validerCommandeClients';
+export const API_validerCommandeClients =
+    'https://e-commerce-simple-backend.onrender.com/validerCommandeClients';
 
-export const API_displayCommandeValider = 'https://e-commerce-simple-backend.onrender.com/displayCommandeValider';
+export const API_displayCommandeValider =
+    'https://e-commerce-simple-backend.onrender.com/displayCommandeValider';
 
-export const API_deleteCommandeValider = 'https://e-commerce-simple-backend.onrender.com/deleteCommandeValider';
+export const API_deleteCommandeValider =
+    'https://e-commerce-simple-backend.onrender.com/deleteCommandeValider';
